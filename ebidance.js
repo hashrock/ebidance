6f711aa20431a85d68d350416f769a2b773a65e4
hashrock <hashedrock@gmail.com>
2015-05-21 16:21:44 +0900

    initial commit

---
+++ ebidance.js
@@ -0,0 +1,42 @@
+Tickets = new Meteor.Collection('tickets');
+
+if (Meteor.isClient) {
+  Template.main.rendered = function(){
+    var vm = new Vue({
+      el: "#main",
+      data: {
+        title: "Test #1",
+        name: "",
+        tickets: [
+            {name: "test"},
+            {name: "test1"},
+            {name: "test2"}
+        ]
+      },
+      sync: {
+        'tickets': function() {
+            return Tickets.find();
+        }
+      },
+      methods: {
+        addTicket: function(e){
+          e.preventDefault();
+          Tickets.insert({name: this.name, status:""});
+          this.name ="";
+        },
+        setStatus: function(id, status){
+          Tickets.update(id, {$set: {
+            status: status
+          }});
+        }
+      }
+    });
+  }
+  
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
